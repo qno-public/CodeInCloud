22a3aa703df5d4139f7a17e47be77a20455269f2
Artjoms Savostjanovs <qno.public@gmail.com>
2015-05-11 21:56:34 +0300

    Sources added to nav

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -34,12 +34,10 @@ AppAsset::register($this);
             ]);
             $menuItems = [
                 ['label' => 'Home', 'url' => ['/site/index']],
+                ['label'=>'Sources', 'url'=> ['source/index'], 'items'=> []],
                 ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-                ['label'=>'Products', 'url'=> ['product/index'], 'items'=> [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ]]
+                ['label' => 'Contact', 'url' => ['/site/contact']]
+
             ];
             if (Yii::$app->user->isGuest) {
                 $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
