7bbd267b16b9899e554ad003e8b761f0775d148c
Artjoms Savostjanovs <qno.public@gmail.com>
2015-05-12 23:08:52 +0300

    source controller fixed

--- frontend/controllers/SourceController.php
+++ frontend/controllers/SourceController.php
@@ -4,14 +4,11 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use app\models\Source;
-use devgroup\ace\AceHelper
 
 class SourceController extends Controller
 {
 	public function actionIndex()
     {
-    	
-
         $source = new Source;
         $data = $source->find()->all();
         echo $this->render('sources', array(
